e1e8b94aacd38aa5a685b1cb1b4fe98c7974ea26
serhii-hryhorenko <serhii.hryhorenko@teamdev.com>
2024-04-01 10:37:30 +0300

    Add docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -2,10 +2,20 @@ import pandas as pd
 
 
 def read_input(prompt: str) -> str:
+    """
+    Read input from the user
+    :param prompt:
+    :return:
+    """
     return input(f'{prompt}:')
 
 
 def read_file(file: str) -> str:
+    """
+    Read file content
+    :param file: path to file
+    :return: file content
+    """
     with open(file, 'r') as f:
         return f.read()
 
--- app/io/output.py
+++ app/io/output.py
@@ -2,10 +2,23 @@ from datetime import datetime
 
 
 def log(message: str):
+    """
+    Log a message with a timestamp
+    :param message: a message to log
+    :return: None
+    """
+
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f'[{timestamp}] - {message}')
 
 
 def write_file(file: str, content: str):
+    """
+    Write content to a file
+    :param file: path to file
+    :param content: content to write
+    :return: None
+    """
+
     with open(file, 'w') as f:
         f.write(content)
